293ba0127922f9720f1d69b8103a5172e32058c6
Haf <haf@protonmail.ch>
2023-08-08 06:56:56 +0200

    002 level 1 go

---
+++ 002-fizz-buzz/level1/go/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "strconv"
+)
+
+func main() {
+  input, err := strconv.Atoi(os.Args[1])
+
+  if err != nil {
+    fmt.Println(err.Error())
+    os.Exit(1)
+  }
+
+  switch {
+  case input % 15 == 0:
+    fmt.Println("FizzBuzz")
+  case input % 3 == 0:
+    fmt.Println("Fizz")
+  case input % 5 == 0:
+    fmt.Println("Buzz")
+  default:
+    fmt.Println(input)
+  }
+  
+}
