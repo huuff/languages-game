f49ee552bdd7b4cac94da07080146ffc3c9be912
Haf <haf@protonmail.ch>
2023-08-09 18:32:01 +0200

    some adjustments and level4

---
+++ 001-hello-world/level4/go/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "strings"
+)
+
+type greeterFunction func(name string) string 
+type greetersMap map[string]greeterFunction
+
+func defaultString(input, defaultValue string) string {
+  if input != "" {
+    return input
+  } else {
+    return defaultValue
+  }
+}
+
+func newHelloHandler(greeters greetersMap) func(w http.ResponseWriter, req *http.Request) {
+  return func(w http.ResponseWriter, req *http.Request) {
+    language := req.Header.Get("Accept-Language")
+    if language == "" {
+      language = "en"
+    }
+    greeter, ok := greeters[language]
+    if !ok {
+      // TODO: Return a bad request
+      fmt.Println(fmt.Sprintf("Unrecognized language %s", language))
+    }
+
+    name := strings.TrimPrefix(req.URL.Path, "/hello/")
+
+    fmt.Println(fmt.Sprintf("Language: %s", language))
+    fmt.Println(fmt.Sprintf("Name: %s", name))
+    fmt.Fprintf(w, greeter(name) + "\n")
+  }
+}
+
+func main() {
+  // TODO: Use an iota for the language values (also in level3)
+  greeters := greetersMap {
+    "es": func(name string) string { 
+      return fmt.Sprintf("¡Hola, %s!", defaultString(name, "Mundo"))
+    },
+    "en": func(name string) string { 
+      return fmt.Sprintf("Hello, %s!", defaultString(name, "World"))
+    },
+    "fr": func(name string) string {
+      return fmt.Sprintf("Bonjour, %s!", defaultString(name, "le Monde"))
+    },
+    "it": func(name string) string { 
+      return fmt.Sprintf("Ciao, %s!", defaultString(name, "Mondo"))
+    },
+    "de": func(name string) string {
+      return fmt.Sprintf("Hallo, %s!", defaultString(name, "Welt"))
+    },
+  }
+
+  // TODO: This is using the default server right? Create a new one
+  // TODO: Won't work without a name or trailing slash
+  // Use a library for it
+  http.HandleFunc("/hello/", newHelloHandler(greeters))
+  http.ListenAndServe(":3333", nil)
+}
--- README.md
+++ README.md
@@ -13,7 +13,7 @@ It takes two command-line arguments:
 1. A name to greet.
 2. A language code in ISO 639-1. One of: `en`, `es`, `fr`, `de` or `it`. Choose english if not present.
 
-Greet the user using the name of the second argument (or `World` if not present) in the provided language. The salutations in each language are:
+Greet the user using the name of the second argument (or the language equivalent of `World` if not present) in the provided language. The salutations in each language are:
 
 * `en`: `Hello, «name»!` or `Hello, World!`
 * `es`: `¡Hola, «name»!` or `¡Hola, Mundo!`
@@ -22,8 +22,16 @@ Greet the user using the name of the second argument (or `World` if not present)
 * `it`: `Ciao, «name»!` or `Ciao, Mondo!`
 
 ##### Learning opportunities
-
 * If your programming language has enumerations, they are a good fit to represent locale codes. Also, you can investigate how to turn a locale string provided as an argument into an enumeration value.
+
+##### Level 4
+Do it as a web service that:
+
+* Receives the language in the `Accept-Language` header. If no language is received, default to english. If more than one language is provided, use only the first one.
+* On a `GET` request to `/hello/«name»` returns a `200` response with contents `Hello, «name»!` (or the equivalent in the provided language) as a `text/plain; charset=utf-8` in the chosen language. 
+* On a `GET` to `/hello` (without a `«name»`) return `Hello World!` in the chosen language.
+* Return the chosen language (or english, if none is provided) in the `Content-Language` header.
+* When the provided language does not exist, return a `400` status response with content: `Unrecognized language: «language»`
 
 ### 002. Fizz Buzz
 #### Level 1
