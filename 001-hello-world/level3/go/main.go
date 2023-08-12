11d45407beeca4073ab3a13e947b79fc466221b9
Haf <haf@protonmail.ch>
2023-08-12 11:27:58 +0200

    refactored level3

--- 001-hello-world/level3/go/main.go
+++ 001-hello-world/level3/go/main.go
@@ -3,41 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"hello/level3/language"
 )
 
-type greeterFunction func(name string) string
-
-func defaultString(input, defaultValue string) string {
-	if input != "" {
-		return input
-	} else {
-		return defaultValue
-	}
-}
-
 func main() {
-	greeters := map[language.Language]greeterFunction{
-		language.Spanish: func(name string) string {
-			return fmt.Sprintf("¡Hola, %s!", defaultString(name, "Mundo"))
-		},
-		language.English: func(name string) string {
-			return fmt.Sprintf("Hello, %s!", defaultString(name, "World"))
-		},
-		language.French: func(name string) string {
-			return fmt.Sprintf("Bonjour, %s!", defaultString(name, "le Monde"))
-		},
-		language.Italian: func(name string) string {
-			return fmt.Sprintf("Ciao, %s!", defaultString(name, "Mondo"))
-		},
-		language.German: func(name string) string {
-			return fmt.Sprintf("Hallo, %s!", defaultString(name, "Welt"))
-		},
-	}
-
 	var name string
-	var lang language.Language
+	var lang Language
 
 	if len(os.Args) > 1 {
 		name = os.Args[1]
@@ -45,9 +15,9 @@ func main() {
 		name = ""
 	}
 	if len(os.Args) > 2 {
-		lang, _ = language.Parse(os.Args[2])
+		lang, _ = Parse(os.Args[2])
 	} else {
-		lang = language.English
+		lang = English
 	}
 
 	greet, ok := greeters[lang]
