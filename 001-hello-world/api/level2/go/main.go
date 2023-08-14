8499db18e4ed7360269caf377e565c46fae1f569
Haf <haf@protonmail.ch>
2023-08-14 07:06:08 +0200

    wip level 2

---
+++ 001-hello-world/api/level2/go/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "net/http"
+  "fmt"
+)
+
+func handleName(w http.ResponseWriter, r *http.Request) {
+  w.WriteHeader(http.StatusOK)
+  
+  name := r.URL.Path
+  fmt.Fprintf(w, "Hello, %s!\n", name)
+}
+
+func main() {
+  mux := http.NewServeMux()
+  mux.Handle("/hello/", http.StripPrefix("/hello/", http.HandlerFunc(handleName)))
+  
+  server := http.Server {
+    Addr: ":8080",
+    Handler: mux,
+  }
+
+  if err := server.ListenAndServe(); err != nil {
+    panic(err)
+  }
+}
