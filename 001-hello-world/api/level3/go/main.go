8642311acdea8a260cc44843ebfb1b40a0ef1c80
Haf <haf@protonmail.ch>
2023-08-15 10:09:27 +0200

    did level3 but by copy-pasting

--- 001-hello-world/api/level3/go/main.go
+++ 001-hello-world/api/level3/go/main.go
@@ -20,6 +20,19 @@ func getLanguage(r *http.Request) (Language, error) {
   }
 }
 
+func handleHelloNoName(w http.ResponseWriter, r *http.Request) {
+  language, err := getLanguage(r)
+  if err != nil {
+    w.WriteHeader(http.StatusBadRequest)
+    fmt.Fprintf(w, "%s\n", err.Error())
+    return
+  }
+
+  w.Header().Set("content-language", string(language))
+  w.WriteHeader(http.StatusOK)
+  fmt.Fprintf(w, "%s\n", getSalutation("", language))
+}
+
 func handleHelloName(w http.ResponseWriter, r *http.Request) {
   name := r.URL.Path
   language, err := getLanguage(r)
@@ -36,6 +49,7 @@ func handleHelloName(w http.ResponseWriter, r *http.Request) {
 
 func main() {
   mux := http.NewServeMux()
+  mux.HandleFunc("/hello", handleHelloNoName)
   mux.Handle("/hello/", http.StripPrefix("/hello/", http.HandlerFunc(handleHelloName)))
 
   server := http.Server {
